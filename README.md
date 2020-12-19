98b8ad8963e66b10fec93a683d8ac655b9ec07c5
GhoulYaev <ghoulyaev@gmail.com>
2020-12-19 23:43:17 +0300

    first v

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dotfiles
 my configs for sway
+for installing start the install.sh
+
---
+++ install.sh
@@ -0,0 +1,8 @@
+#installing configuration 
+cp configs/alacritty ~/.config/ -r
+cp configs/neofetch ~/.config/ -r
+cp configs/sway/ ~/.config/ -r
+cp configs/wofi ~/.config/ -r
+cp configs/waybar ~/.config/ -r
+cp wallpaper/ ~/ -r
+echo "succesful! please reboot the system"
