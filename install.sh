1243e76ffc11051eee560f4855c4279afc339e50
ghoulyaev <68611633+GhoulYaev@users.noreply.github.com>
2021-04-13 10:14:00 +0300

    поправил скрипт

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #installing configuration 
-cp configs/alacritty ~/.config/ -r
-cp configs/neofetch ~/.config/ -r
-cp configs/sway/ ~/.config/ -r
-cp configs/wofi ~/.config/ -r
-cp configs/waybar ~/.config/ -r
-cp wallpaper/ ~/ -r
-echo "succesful! please reboot the system"
+cp configs/* ~/.config/ -r
+cp wallpaper/ ~/
+echo "succesful!"
